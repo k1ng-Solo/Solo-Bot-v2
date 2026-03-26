42021ac9d5a6bfa0ebb5c54e16832ce5b09dea7d
King-Solo <nsoforjoe@Gmail.com>
2026-03-26 19:53:24 +0100

    error ignore

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,16 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const { handleMessage } = require('/handler/MessageHandler');
-const SessionManager = require('./utils/SessionManager');
+const { handleMessage } = require('./handler/MessageHandler');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: true }
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
 
 client.on('ready', () => {
-    console.log('🤖 WhatsApp Business Bot is online!');
+    console.log('✅ Bot is ready!');
 });
 
 client.on('message', async message => {
